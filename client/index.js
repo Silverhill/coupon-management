3f51cda7bbee4a33b42b3609fd77248c8a6b9502
Fabricio Flores <fabrifloresg@gmail.com>
2017-12-18 17:18:04 -0500

    New stack

--- client/index.js
+++ client/index.js
@@ -1,34 +1,24 @@
-/**
- * Client entry point
- */
-import React from 'react';
-import { render } from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
-import App from './App';
-import { configureStore } from './store';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
+import App from './components/App'
 
-// Initialize store
-const store = configureStore(window.__INITIAL_STATE__);
-const mountApp = document.getElementById('root');
-
-render(
+ReactDOM.render(
   <AppContainer>
-    <App store={store} />
+    <App/>
   </AppContainer>,
-  mountApp
+  document.getElementById('root')
 );
 
-// For hot reloading of react components
+// Hot Module Replacement API
 if (module.hot) {
-  module.hot.accept('./App', () => {
-    // If you use Webpack 2 in ES modules mode, you can
-    // use <App /> here rather than require() a <NextApp />.
-    const NextApp = require('./App').default; // eslint-disable-line global-require
-    render(
+  module.hot.accept('./components/App', () => {
+    const NextApp = require('./components/App').default;
+    ReactDOM.render(
       <AppContainer>
-        <NextApp store={store} />
+        <NextApp/>
       </AppContainer>,
-      mountApp
+      document.getElementById('root')
     );
   });
 }
--- server/server.js
+++ server/server.js
@@ -1,149 +1,66 @@
-import Express from 'express';
-import compression from 'compression';
+import express from 'express';
+//import open from 'open';
 import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
-import path from 'path';
-import IntlWrapper from '../client/modules/Intl/IntlWrapper';
-
-// Webpack Requirements
 import webpack from 'webpack';
 import config from '../webpack.config.dev';
-import webpackDevMiddleware from 'webpack-dev-middleware';
-import webpackHotMiddleware from 'webpack-hot-middleware';
+import home from './routes/home';
+import user from './routes/user';
 
-// Initialize the Express App
-const app = new Express();
+/* eslint-disable no-console */
 
-// Run Webpack dev server in development mode
-if (process.env.NODE_ENV === 'development') {
-  const compiler = webpack(config);
-  app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
-  app.use(webpackHotMiddleware(compiler));
-}
+const port = 3000;
+const mongoURL = process.env.MONGO_URL || 'mongodb://localhost:27017/coupon-db';
+const app = express();
+const compiler = webpack(config);
 
-// React And Redux Setup
-import { configureStore } from '../client/store';
-import { Provider } from 'react-redux';
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-import { match, RouterContext } from 'react-router';
-import Helmet from 'react-helmet';
+let v1 = express.Router()
 
-// Import required modules
-import routes from '../client/routes';
-import { fetchComponentData } from './util/fetchData';
-import posts from './routes/post.routes';
-import dummyData from './dummyData';
-import serverConfig from './config';
-
-// Set native promises as mongoose promise
 mongoose.Promise = global.Promise;
 
-// MongoDB Connection
-mongoose.connect(serverConfig.mongoURL, (error) => {
-  if (error) {
-    console.error('Please make sure Mongodb is installed and running!'); // eslint-disable-line no-console
-    throw error;
-  }
-
-  // feed some dummy data in DB.
-  dummyData();
+mongoose.connect(mongoURL, { useMongoClient: true }, (error) => {
+    if (error) {
+        console.error('Please make sure Mongodb is installed and running!');
+        throw error;
+    }
 });
 
-// Apply body Parser and server public assets and routes
-app.use(compression());
-app.use(bodyParser.json({ limit: '20mb' }));
-app.use(bodyParser.urlencoded({ limit: '20mb', extended: false }));
-app.use(Express.static(path.resolve(__dirname, '../dist/client')));
-app.use('/api', posts);
-
-// Render Initial HTML
-const renderFullPage = (html, initialState) => {
-  const head = Helmet.rewind();
-
-  // Import Manifests
-  const assetsManifest = process.env.webpackAssets && JSON.parse(process.env.webpackAssets);
-  const chunkManifest = process.env.webpackChunkAssets && JSON.parse(process.env.webpackChunkAssets);
-
-  return `
-    <!doctype html>
-    <html>
-      <head>
-        ${head.base.toString()}
-        ${head.title.toString()}
-        ${head.meta.toString()}
-        ${head.link.toString()}
-        ${head.script.toString()}
-
-        ${process.env.NODE_ENV === 'production' ? `<link rel='stylesheet' href='${assetsManifest['/app.css']}' />` : ''}
-        <link href='https://fonts.googleapis.com/css?family=Lato:400,300,700' rel='stylesheet' type='text/css'/>
-        <link rel="shortcut icon" href="http://res.cloudinary.com/hashnode/image/upload/v1455629445/static_imgs/mern/mern-favicon-circle-fill.png" type="image/png" />
-      </head>
-      <body>
-        <div id="root">${html}</div>
-        <script>
-          window.__INITIAL_STATE__ = ${JSON.stringify(initialState)};
-          ${process.env.NODE_ENV === 'production' ?
-          `//<![CDATA[
-          window.webpackManifest = ${JSON.stringify(chunkManifest)};
-          //]]>` : ''}
-        </script>
-        <script src='${process.env.NODE_ENV === 'production' ? assetsManifest['/vendor.js'] : '/vendor.js'}'></script>
-        <script src='${process.env.NODE_ENV === 'production' ? assetsManifest['/app.js'] : '/app.js'}'></script>
-      </body>
-    </html>
-  `;
-};
-
-const renderError = err => {
-  const softTab = '&#32;&#32;&#32;&#32;';
-  const errTrace = process.env.NODE_ENV !== 'production' ?
-    `:<br><br><pre style="color:red">${softTab}${err.stack.replace(/\n/g, `<br>${softTab}`)}</pre>` : '';
-  return renderFullPage(`Server Error${errTrace}`, {});
-};
-
-// Server Side Rendering based on routes matched by React-router.
-app.use((req, res, next) => {
-  match({ routes, location: req.url }, (err, redirectLocation, renderProps) => {
-    if (err) {
-      return res.status(500).end(renderError(err));
-    }
-
-    if (redirectLocation) {
-      return res.redirect(302, redirectLocation.pathname + redirectLocation.search);
-    }
-
-    if (!renderProps) {
-      return next();
-    }
-
-    const store = configureStore();
-
-    return fetchComponentData(store, renderProps.components, renderProps.params)
-      .then(() => {
-        const initialView = renderToString(
-          <Provider store={store}>
-            <IntlWrapper>
-              <RouterContext {...renderProps} />
-            </IntlWrapper>
-          </Provider>
-        );
-        const finalState = store.getState();
-
-        res
-          .set('Content-Type', 'text/html')
-          .status(200)
-          .end(renderFullPage(initialView, finalState));
-      })
-      .catch((error) => next(error));
-  });
+mongoose.connection.once('open', function() {
+    console.log('Mongodb connection success!!');
 });
 
-// start app
-app.listen(serverConfig.port, (error) => {
-  if (!error) {
-    console.log(`MERN is running on port: ${serverConfig.port}! Build something amazing!`); // eslint-disable-line
-  }
+if (process.env.NODE_ENV !== 'test') {
+    app.use(require('webpack-dev-middleware')(compiler, {
+        noInfo: true,
+        publicPath: config.output.publicPath
+    }));
+    app.use(require("webpack-hot-middleware")(compiler));
+}
+
+// Api routes
+v1.use('/', home);
+v1.use('/user', user);
+
+// Api version
+app.use('/v1', v1);
+// Default API Version
+app.use('/', v1);
+
+// catch 404 and forward to error handler
+app.use((req, res) => {
+    res.status(404).json({
+        message: 'Route not found'
+    })
+})
+
+app.listen(port, function (error) {
+    if(error) {
+        console.log(error);
+    } else {
+        console.log(`Express App running on port ${port}`);
+        if (process.env.NODE_ENV !== 'test') {
+            //open(`http://localhost:${port}`)
+        }
+    }
 });
 
 export default app;
