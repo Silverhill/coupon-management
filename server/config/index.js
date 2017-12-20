a55b796d56413344e79b0e6a328b763b41330774
Fabricio Flores <fabrifloresg@gmail.com>
2017-12-20 17:15:50 -0500

    Fix eslint

--- server/config/index.js
+++ server/config/index.js
@@ -1,11 +1,11 @@
 export default {
   facebook:{
-    appId: process.env.FBId || '302305353609153',
-    secret: process.env.FBSecret || '2632322f05241cb2d07fa31765e3c5d4'
+    appId: process.env.FBId ||'302305353609153',
+    secret: process.env.FBSecret ||'2632322f05241cb2d07fa31765e3c5d4'
   },
   google:{
-    appId: process.env.GoogleId || '705966156450-mq8j847ugrl37jk4jraggf279qvi4qil.apps.googleusercontent.com',
-    secret: process.env.GoogleSecret || 'VInnQimkPxfurIeBGWY0m6V4'
+    appId: process.env.GoogleId ||'705966156450-mq8j847ugrl37jk4jraggf279qvi4qil.apps.googleusercontent.com',
+    secret: process.env.GoogleSecret ||'VInnQimkPxfurIeBGWY0m6V4'
   },
-  mailToken: process.env.MailToken || 'dfksaldjfklsajfdklasddkfjas'
+  mailToken: process.env.MailToken ||'dfksaldjfklsajfdklasddkfjas'
 }
