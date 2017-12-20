97d009a122ef0048de51d71d35f07122b45ab06d
Fabricio Flores <fabrifloresg@gmail.com>
2017-12-20 17:12:33 -0500

    Update with development tokens

--- server/config/index.js
+++ server/config/index.js
@@ -1,11 +1,11 @@
 export default {
   facebook:{
-    apiKey: process.env.FBApiKey || 'dfksaldjfklsajfdklasddkfjas',
-    secret: process.env.FBSecret || 'dfksaldjfklsajfdklasddkfjas'
+    appId: process.env.FBId || '302305353609153',
+    secret: process.env.FBSecret || '2632322f05241cb2d07fa31765e3c5d4'
   },
   google:{
-    apiKey: process.env.GoogleApiKey || 'dfksaldjfklsajfdklasddkfjas',
-    secret: process.env.GoogleSecret || 'dfksaldjfklsajfdklasddkfjas'
+    appId: process.env.GoogleId || '705966156450-mq8j847ugrl37jk4jraggf279qvi4qil.apps.googleusercontent.com',
+    secret: process.env.GoogleSecret || 'VInnQimkPxfurIeBGWY0m6V4'
   },
   mailToken: process.env.MailToken || 'dfksaldjfklsajfdklasddkfjas'
 }
