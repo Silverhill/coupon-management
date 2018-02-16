8aee5541a436b54d9e303a6a1e8ba64293a53744
Jhymer Martínez <jmartinez@ioet.com>
2018-02-16 15:54:29 -0500

    Add React Intl

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { FormattedMessage } from 'react-intl'
 
 class HomePage extends React.Component {
   render() {
     return (
       <div>
-        <h1>Home Page</h1>
+        <h1>
+          <FormattedMessage id='MANAGEMENT.home'/>
+        </h1>
       </div>
     );
   }
