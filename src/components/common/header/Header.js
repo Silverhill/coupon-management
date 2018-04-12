780a285d7b563cd59fceacdb5543e85e654c1c1a
Fabricio Flores <fabrifloresg@gmail.com>
2018-04-11 19:46:06 -0500

    Add header

--- src/components/common/header/Header.js
+++ src/components/common/header/Header.js
@@ -1,61 +1,72 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { Avatar,
+         Typography,
+         Icon,
+         Dropdown,
+         DropdownTrigger,
+         DropdownContent } from 'coupon-components';
 import PropTypes from 'prop-types';
-import {NavLink} from 'react-router-dom';
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
-import * as sessionActions from '../../../actions/sessionActions';
+import styles from './header.scss';
+import Logo from 'Components/common/logo/Logo';
 
 class Header extends React.Component {
-  constructor() {
-    super();
-    this.logOut = this.logOut.bind(this);
-  }
-
-  logOut(event) {
-    event.preventDefault();
-    this.props.actions.logOutUser();
-  }
 
   render() {
-    if (this.props.logged_in) {
-      return (
-        <nav>
-          <NavLink to="/" activeClassName="active">Home</NavLink>
-          {" | "}
-          <NavLink to="/hunters" activeClassName="active">Hunters</NavLink>
-          {" | "}
-          <NavLink to="/makers" activeClassName="active">Makers</NavLink>
-          {" | "}
-          <a href="/logout" onClick={this.logOut}>log out</a>
-        </nav>
-      );
-    } else {
-      return (
-        <nav>
-          <NavLink to="/" activeClassName="active">Home</NavLink>
-          {" | "}
-          <NavLink to="/login" activeClassName="active">log in</NavLink>
-        </nav>
-      );
-    }
+    const primaryColor = '#fefefe';
+    const { tabs, userData } = this.props;
+
+    return (
+      <div className={styles.container}>
+        <div className={styles.logo}>
+          <NavLink to='/'>
+            <Logo color={primaryColor} />
+            <div className={styles.dividerLogo}></div>
+            <Typography.Subtitle light style={{ color: primaryColor }}>MAKER</Typography.Subtitle>
+          </NavLink>
+        </div>
+        <div className={styles.itemsContainer}>
+          {tabs && tabs.map((tab) => {
+            const key = { key: tab.id };
+            return (
+              <NavLink {...key} to={tab.route} className={styles.tabs} activeClassName={styles.active}>
+                <Icon name={tab.icon} size={30} />
+                <Typography.Text small bold>{tab.label}</Typography.Text>
+              </NavLink>
+            )
+          })}
+          <Dropdown>
+            <DropdownTrigger>
+              <div className={styles.userMenu}>
+                <div className={styles.avatarContainer}>
+                  <Avatar image={userData.image}
+                    borderColor={primaryColor} />
+                </div>
+                <Typography.Text small bold>{userData.name}</Typography.Text>
+                <Icon name="FaCaretDown" size={10} />
+              </div>
+            </DropdownTrigger>
+            <DropdownContent className={styles.menuContainer}>
+              {userData.options && userData.options.map((option, i) => {
+                return (
+                  <div key={`option-${i}`} onClick={option.onClick} className={styles.menuOption}>
+                    <Typography.Text small>
+                      {option.value}
+                    </Typography.Text>
+                  </div>
+                )
+              })}
+            </DropdownContent>
+          </Dropdown>
+        </div>
+      </div>
+    );
   }
 }
 
 Header.propTypes = {
-  actions: PropTypes.object.isRequired,
   userData: PropTypes.object,
   tabs: PropTypes.array,
 }
 
-function mapStateToProps(state) {
-  return {logged_in: state.session};
-}
-
-function mapDispatchToProps(dispatch) {
-  return {
-    actions: bindActionCreators(sessionActions, dispatch)
-  };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Header);
-
+export default Header;
--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,20 +1,74 @@
 import React from 'react';
-import { FormattedMessage } from 'react-intl'
+import { FormattedMessage } from 'react-intl';
 import { Route, Switch } from 'react-router-dom';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as sessionActions from '../../actions/sessionActions';
+import { graphql } from 'react-apollo';
 
 import Header from 'Components/common/header/Header';
 import HunterPage from 'Pages/hunters/HunterPage';
 import MakerPage from 'Pages/makers/MakerPage';
 import Footer from 'Components/common/footer/Footer';
+import QueryService from '../../queries-service';
 
 import styles from './HomePage.scss';
 
 const PageHome = (props) => <div><h1><FormattedMessage id='MANAGEMENT.home' /></h1></div>
 class HomePage extends React.Component {
+
+  constructor() {
+    super();
+    this.logOut = this.logOut.bind(this);
+  }
+
+  logOut(event) {
+    this.props.actions.logOutUser();
+  }
+
   render() {
+    const { data: { error, loading, me }} = this.props;
+    let tabOptions = [
+      {
+        id: 0,
+        label: 'Home',
+        route: '/',
+        icon: 'CpTicket'
+      },
+      {
+        id: 1,
+        label: 'Hunters',
+        route: '/hunters',
+        icon: 'FaListAlt'
+      },
+      {
+        id: 2,
+        label: 'Makers',
+        route: '/makers',
+        icon: 'FaBellO'
+      },
+    ];
+    let userData = {
+      name: me && me.name,
+      image: (me && me.image) || 'https://i.pinimg.com/564x/bc/c8/10/bcc8102f42e58720355ca02d833c204b.jpg',
+      options: [
+        {
+          value: 'Mi perfil',
+          onClick: ()=>{this.props.history.push('/profile');}
+        },
+        {
+          value: 'Cerrar Sesion',
+          onClick: ()=>{
+            this.logOut();
+          }
+        }
+      ]
+    };
+    if(error) return <h4>{error.message}</h4>
+    else if(loading) return <h4>Loading...</h4>
     return (
       <div className={styles.container}>
-        <Header />
+        <Header tabs={tabOptions} userData={userData}/>
         <div className={styles.mainView}>
           <main className={styles.renderContainer}>
             <Switch>
@@ -30,4 +84,16 @@ class HomePage extends React.Component {
   }
 }
 
-export default HomePage;
+function mapStateToProps(state) {
+  return {logged_in: state.session};
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(sessionActions, dispatch)
+  };
+}
+
+const HomePageWithUser = graphql(QueryService.getMe)(HomePage);
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomePageWithUser);
