fbcfbfc16f79e90be69e98c9e53905ec6a58a024
Fabricio Flores <fabrifloresg@gmail.com>
2018-05-06 00:18:44 -0500

    Use env vars

--- src/environment/production.js
+++ src/environment/production.js
@@ -1,2 +1,3 @@
 module.exports = {
+  API_URL: process.env.API_URL
 };
--- src/environment/staging.js
+++ src/environment/staging.js
@@ -1,3 +1,3 @@
 module.exports = {
-  API_URL: 'https://coupon-backend.herokuapp.com/graphql'
+  API_URL: process.env.API_URL
 };
