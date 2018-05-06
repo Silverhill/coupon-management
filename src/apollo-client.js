f7544997499618e7117f4f19802ef89effb86419
Fabricio Flores <fabrifloresg@gmail.com>
2018-05-06 00:01:11 -0500

    Add hook for build

--- src/apollo-client.js
+++ src/apollo-client.js
@@ -3,7 +3,7 @@ import { HttpLink } from 'apollo-link-http';
 import { ApolloLink, concat } from 'apollo-client-preset'
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import config from './environment';
-console.log(config);
+
 const httpLink = new HttpLink({ uri: config.API_URL });
 
 const middlewareAuthLink = new ApolloLink((operation, forward) => {
